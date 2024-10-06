5f352c218e9fd5a8a699b0960b23872f758e22f0
Nouf AL-Zumaia <66878203+noufzumaia@users.noreply.github.com>
2024-10-06 11:39:18 +0300

    Update name app.js

--- server/database/app.js
+++ server/database/app.js
@@ -91,7 +91,7 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 
   const review = new Reviews({
     "id": new_id,
-    "name": data['name'],
+    "name": data["name"],
     "dealership": data['dealership'],
     "review": data['review'],
     "purchase": data['purchase'],
