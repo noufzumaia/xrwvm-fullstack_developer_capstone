5e490b7fb5f6d89677ed3259a5c04b87a9b316b9
Nouf AL-Zumaia <66878203+noufzumaia@users.noreply.github.com>
2024-10-06 11:41:11 +0300

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -92,13 +92,13 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   const review = new Reviews({
     "id": new_id,
     "name": data["name"],
-    "dealership": data['dealership'],
-    "review": data['review'],
-    "purchase": data['purchase'],
-    "purchase_date": data['purchase_date'],
-    "car_make": data['car_make'],
-    "car_model": data['car_model'],
-    "car_year": data['car_year'],
+    "dealership": data["dealership"],
+    "review": data["review"],
+    "purchase": data["purchase"],
+    "purchase_date": data["purchase_date"],
+    "car_make": data["car_make"],
+    "car_model": data["car_model"],
+    "car_year": data["car_year"],
   });
 
   try {
