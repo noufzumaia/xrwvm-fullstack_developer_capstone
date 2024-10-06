9585a52c4080793ae03689025a06f6b343248c71
Nouf AL-Zumaia <66878203+noufzumaia@users.noreply.github.com>
2024-10-06 09:13:43 +0300

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,17 +1,22 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
+
+searchcars_url = os.getenv(
+    'searchcars_url',
+    default="http://localhost:3050/")
+
 
 # def get_request(endpoint, **kwargs):
+# Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
@@ -29,9 +34,10 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("A network exception occurred: ", err)
 
-# Add code for get requests to back end
 
 # def analyze_review_sentiments(text):
+# request_url = sentiment_analyzer_url+"analyze/"+text
+# Add code for retrieving sentiments
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -41,11 +47,8 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
 
 # def post_review(data_dict):
-# Add code for posting review
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
     try:
@@ -55,3 +58,22 @@ def post_review(data_dict):
     except Exception as err:
         print("A network exception occurred: ", err)
 
+# def searchcars_request(endpoint, **kwargs):
+def searchcars_request(endpoint, **kwargs):
+    params = ""
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key + "=" + value + "&"
+
+    request_url = searchcars_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    finally:
+        print("GET request call complete!")
